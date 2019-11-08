968c5eca381fb835a5552fd1d0f9ec0b864c8039
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-11-08 08:27:22 -0500

    Copy pasting from my sequelize version. Had to remove sequelize and build in generic ORM functions

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+// Import the ORM to implement functions that will interact with the database
+let orm = require('../config/orm.js');
+
+// Create burger object
+let burger = {
+  // Select all burger table data
+  selectAll: function(cb) {
+    orm.selectAll('burgers', function(res) {
+      cb(res);
+    });
+  },
+
+  // The variables cols and vals are arrays
+  insertOne: function(vals, cb) {
+    orm.insertOne(vals, function(res) {
+      cb(res);
+    });
+  },
+
+  // The objColVals is an object specifying columns as object keys with associated values
+  updateOne: function(condition, cb) {
+    orm.updateOne(condition, function(res) {
+      cb(res);
+    });
+  }
+};
+// Export the database functions for the controller (burgerController.js).
+module.exports = burger;
